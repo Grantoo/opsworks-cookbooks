081d8dc52f736db24f356dbe771523c59cc4d7f4
mark <mark@grantoo.org>
2013-11-01 14:50:49 -0700

    built from opsworks_agent_monit as an example first pass of launching resque built into our app
    simpler version of grantoo_resque_monit - might work
    
    less code from sample to deploy monit file
    
    fix missing info and try to include @deploy via deploy::deploy
    
    fix missing info and try to include @deploy via deploy::deploy
    
    add @ to deploy... hmmm
    
    debug with yaml
    
    remove debug with yaml
    
    allow restarting once installed
    remove other confusing break-outs
    
    change output so monit will be restarted
    
    monit needs a real group for deploy
    
    monit needs a real group for deploy - last one was backwards systems
    
    create a separate monit for each worker process per cpu
    remove old configs before deploying - in case there is a scaling up or down.
    
    fix bug with notifies - needs to be within template

---
+++ grantoo_resque_monit/recipes/default.rb
@@ -0,0 +1,39 @@
+package "monit"
+include_recipe "deploy"
+
+directory node[:monit][:conf_dir] do
+  group "root"
+  owner "root"
+  action :create
+  recursive true
+end
+
+service "monit" do
+  supports :status => false, :restart => true, :reload => true, :start => true
+  action [:enable, :nothing]
+end
+
+group = node[:monit][:user_group]
+cpu_count = `cat /proc/cpuinfo | grep processor | wc -l`.to_i # assume linux
+
+node[:deploy].each do |application, deploy|
+  Dir.glob(File.join(node[:monit][:conf_dir], "grantoo_resque*.monitrc")).each do |f|
+    File.delete(f)
+  end
+
+  (1..cpu_count).each do | cpu_number |
+    Chef::Log.info("cpu_number #{cpu_number.to_s}")
+    template File.join(node[:monit][:conf_dir], "grantoo_resque_#{cpu_number.to_s}.monitrc") do
+      source "grantoo_resque.monitrc.erb"
+      mode 0644
+      variables(
+          :application => application,
+          :deploy => deploy,
+          :group => group,
+          :worker => cpu_number.to_s
+      )
+      notifies :restart, resources(:service => "monit"), :delayed
+    end
+  end
+
+end
