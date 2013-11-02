e1424ae01abbc10fae7f984a2b01d9a986131301
mark <mark@grantoo.org>
2013-11-01 18:01:48 -0700

    remove debug with yaml

--- grantoo_resque_monit/attributes/default.rb
+++ grantoo_resque_monit/attributes/default.rb
@@ -1,10 +1,10 @@
 include_attribute 'rails::rails'
 
 case node[:platform]
-when 'centos','redhat','fedora','suse','amazon'
-  default[:monit][:conf]     = '/etc/monit.conf'
-  default[:monit][:conf_dir] = '/etc/monit.d'
-when 'debian','ubuntu'
-  default[:monit][:conf]     = '/etc/monit/monitrc'
-  default[:monit][:conf_dir] = '/etc/monit/conf.d'
+  when 'centos', 'redhat', 'fedora', 'suse', 'amazon'
+    default[:monit][:conf] = '/etc/monit.conf'
+    default[:monit][:conf_dir] = '/etc/monit.d'
+  when 'debian', 'ubuntu'
+    default[:monit][:conf] = '/etc/monit/monitrc'
+    default[:monit][:conf_dir] = '/etc/monit/conf.d'
 end
