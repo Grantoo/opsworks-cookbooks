7f6b54168d9fe076b05be5774a06ec645ae8b935
mark <mark@grantoo.org>
2013-11-01 18:01:48 -0700

    remove debug with yaml

--- grantoo_resque_monit/recipes/default.rb
+++ grantoo_resque_monit/recipes/default.rb
@@ -1,4 +1,5 @@
 package "monit"
+include_recipe "deploy"
 
 directory node[:monit][:conf_dir] do
   group "root"
@@ -9,12 +10,15 @@ end
 
 include_recipe "grantoo_resque_monit::service"
 
-template File.join(node[:monit][:conf_dir], "grantoo_resque.monitrc") do
-  require 'yaml'
-  puts node.to_yaml
-  source "grantoo_resque.monitrc.erb"
-  mode 0644
-  #TODO: This should only happen if the service is running, after rebooting
-#  notifies :restart, resources(:service => "monit")
+node[:deploy].each do |application, deploy|
+  template File.join(node[:monit][:conf_dir], "grantoo_resque.monitrc") do
+    source "grantoo_resque.monitrc.erb"
+    mode 0644
+    variables(
+        :application => application,
+        :deploy => deploy
+    )
+    #TODO: This should only happen if the service is running, after rebooting
+    #notifies :restart, resources(:service => "monit")
+  end
 end
-
