052d2eb7a4d70cf6cd6fedfd86ff0d2255b2ec02
mark <mark@grantoo.org>
2013-11-04 10:30:43 -0800

    change output so monit will be restarted
    monit needs a real group for deploy
    
    monit needs a real group for deploy - last one was backwards systems
    
    create a separate monit for each worker process per cpu
    remove old configs before deploying - in case there is a scaling up or down.

--- grantoo_resque_monit/attributes/default.rb
+++ grantoo_resque_monit/attributes/default.rb
@@ -4,7 +4,9 @@ case node[:platform]
   when 'centos', 'redhat', 'fedora', 'suse', 'amazon'
     default[:monit][:conf] = '/etc/monit.conf'
     default[:monit][:conf_dir] = '/etc/monit.d'
+    default[:monit][:user_group] = node['opsworks']['rails_stack']['name'] == 'nginx_unicorn' ? 'nginx' : 'apache'
   when 'debian', 'ubuntu'
     default[:monit][:conf] = '/etc/monit/monitrc'
     default[:monit][:conf_dir] = '/etc/monit/conf.d'
+    default[:monit][:user_group] = 'www-data'
 end
--- grantoo_resque_monit/recipes/default.rb
+++ grantoo_resque_monit/recipes/default.rb
@@ -13,14 +13,25 @@ service "monit" do
   action [:enable, :nothing]
 end
 
+group = node[:monit][:user_group]
+cpu_count = `cat /proc/cpuinfo | grep processor | wc -l`.to_i # assume linux
+
 node[:deploy].each do |application, deploy|
-  template File.join(node[:monit][:conf_dir], "grantoo_resque.monitrc") do
-    source "grantoo_resque.monitrc.erb"
-    mode 0644
-    variables(
-        :application => application,
-        :deploy => deploy
-    )
+  Dir.glob(File.join(node[:monit][:conf_dir], "grantoo_resque*.monitrc")).each do |f|
+    File.delete(f)
+  end
+
+  (1..cpu_count).each do | cpu_number |
+    template File.join(node[:monit][:conf_dir], "grantoo_resque_#{cpu_number}.monitrc") do
+      source "grantoo_resque.monitrc.erb"
+      mode 0644
+      variables(
+          :application => application,
+          :deploy => deploy,
+          :group => group,
+          :worker => cpu_number
+      )
+    end
     notifies :restart, resources(:service => "monit")
   end
 end
