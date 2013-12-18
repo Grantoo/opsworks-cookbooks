ba5860ecc0f8a10355134cb3b1ed6c5a66fb5598
Kirk <chen.kirk@gmail.com>
2013-12-18 11:28:05 -0800

    do not use worker # when removing worker pid
    Do not use worker # when removing worker pid
    More logs

--- grantoo_resque_monit/recipes/kill.rb
+++ grantoo_resque_monit/recipes/kill.rb
@@ -1,5 +1,5 @@
 log "grantoo_resque_monit::kill: pkill -QUIT -f resque"
-`pkill -QUIT -f resque`
+log `pkill -QUIT -f resque`
 
-log "grantoo_resque_monit::kill: rm -f \"<%= @deploy[:deploy_to] %>/shared/pids/grantoo_resque_#{@worker}.pid\""
-`rm -f "<%= @deploy[:deploy_to] %>/shared/pids/grantoo_resque_<%= @worker %>.pid"`
+log "grantoo_resque_monit::kill: rm -f \"#{@deploy[:deploy_to]}/shared/pids/grantoo_resque_*.pid\""
+log `rm -f "<%= @deploy[:deploy_to] %>/shared/pids/grantoo_resque_*.pid"`
