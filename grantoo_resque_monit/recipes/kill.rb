ad3b34f7a718097589d6ce6f534c83030b8351df
Kirk <chen.kirk@gmail.com>
2013-12-18 11:05:45 -0800

    Tried chef log
    Fix syntax

--- grantoo_resque_monit/recipes/kill.rb
+++ grantoo_resque_monit/recipes/kill.rb
@@ -1,5 +1,5 @@
-Rails.logger.info("grantoo_resque_monit::kill: pkill -QUIT -f resque")
+log "grantoo_resque_monit::kill: pkill -QUIT -f resque"
 `pkill -QUIT -f resque`
 
-Rails.logger.info("grantoo_resque_monit::kill: rm -f \"<%= @deploy[:deploy_to] %>/shared/pids/grantoo_resque_#{@worker}.pid\"")
+log "grantoo_resque_monit::kill: rm -f \"<%= @deploy[:deploy_to] %>/shared/pids/grantoo_resque_#{@worker}.pid\""
 `rm -f "<%= @deploy[:deploy_to] %>/shared/pids/grantoo_resque_<%= @worker %>.pid"`
