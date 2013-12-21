b904a247a3a5e1b8f66d12131509b9425ec2507f
kirk <kirk@kirk-Linux-Mint>
2013-12-21 11:38:28 -0800

    Updated log message for kill script Cloned kill script to be name stopped for better clarification Added start script

--- grantoo_resque_monit/recipes/kill.rb
+++ grantoo_resque_monit/recipes/kill.rb
@@ -1,9 +1,10 @@
+# deprecated. use stop.rb instead
 cpu_count = `cat /proc/cpuinfo | grep processor | wc -l`.to_i # assume linux
-Chef::Log.info "grantoo_resque_monit::kill starting with cpu_count #{cpu_count}"
+Chef::Log.info "[grantoo_resque_monit::kill] cpu_count #{cpu_count}"
 
 (1..cpu_count).each do | cpu_number |
-  Chef::Log.info "grantoo_resque_monit::kill stopping resque_worker_#{cpu_number}"
+  Chef::Log.info "[grantoo_resque_monit::kill] stopping resque_worker_#{cpu_number}"
   `monit stop resque_worker_#{cpu_number}`
 end
 
-Chef::Log.info "grantoo_resque_monit::kill done"
+Chef::Log.info "[grantoo_resque_monit::kill] done"
