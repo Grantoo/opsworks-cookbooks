422472af82c6a656acfd861a546415f9564857ba
mark <mark@grantoo.org>
2014-03-03 14:48:23 -0800

    do we need the full path for bundle to work?

--- rails_build_assets/recipes/default.rb
+++ rails_build_assets/recipes/default.rb
@@ -9,6 +9,6 @@ node[:deploy].each do |application, deploy|
   script "build assets" do
     user 'deploy'
     cwd deploy[:deploy_to] + "/current"
-    code "RAILS_ENV=#{deploy[:rails_env]} bundle exec rake assets:precompile"
+    code "RAILS_ENV=#{deploy[:rails_env]} /usr/local/bin/bundle exec rake assets:precompile"
   end
 end
