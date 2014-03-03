e63cd1ff3f0bbcd2a8715c795b43895638cb75a8
mark <mark@grantoo.org>
2014-03-03 11:17:07 -0800

    add a way to build assets for projects that need it

---
+++ rails_build_assets/recipes/default.rb
@@ -0,0 +1,14 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping unicorn::rails application #{application} as it is not an Rails app")
+    next
+  end
+
+  script "build assets" do
+    user 'deploy'
+    cwd deploy[:deploy_to] + "/current"
+    code "RAILS_ENV=#{deploy[:rails_env]} bundle exec rake assets:precompile"
+  end
+end
