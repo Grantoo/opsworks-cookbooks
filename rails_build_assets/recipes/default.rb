d2b35c6ada60b87f573d689d110b271fa07151da
mark <mark@grantoo.org>
2014-03-03 17:19:22 -0800

    use bash directly to run code

--- rails_build_assets/recipes/default.rb
+++ rails_build_assets/recipes/default.rb
@@ -6,14 +6,9 @@ node[:deploy].each do |application, deploy|
     next
   end
 
-  script "build assets" do
-    Chef::Log.debug("set to user 'deploy'")
+  bash "build_assets" do
     user 'deploy'
-    Chef::Log.debug("set to interpreter 'bash'")
-    interpreter 'bash'
-    Chef::Log.debug("set to cwd #{deploy[:deploy_to]}/current")
     cwd deploy[:deploy_to] + "/current"
-    Chef::Log.debug("RAILS_ENV=#{deploy[:rails_env]} /usr/local/bin/bundle exec rake assets:precompile")
-    Chef::Log.debug(`RAILS_ENV=#{deploy[:rails_env]} /usr/local/bin/bundle exec rake assets:precompile`)
+    code "RAILS_ENV=#{deploy[:rails_env]} /usr/local/bin/bundle exec rake assets:precompile"
   end
 end
