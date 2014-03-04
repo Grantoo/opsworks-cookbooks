f9775f5ab5b7ff306cdfb54928554bcbda4f38af
mark <mark@grantoo.com>
2014-03-03 21:40:12 -0800

    restart to unicorn after assets are rebuilt

--- rails_build_assets/recipes/default.rb
+++ rails_build_assets/recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe "deploy"
+include_recipe "unicorn::rails"
 
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
@@ -10,5 +11,9 @@ node[:deploy].each do |application, deploy|
     user 'deploy'
     cwd deploy[:deploy_to] + "/current"
     code "RAILS_ENV=#{deploy[:rails_env]} /usr/local/bin/bundle exec rake assets:precompile"
+    notifies :restart, resources(:service => "unicorn_#{application}")
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config/unicorn.conf")
+    end
   end
 end
