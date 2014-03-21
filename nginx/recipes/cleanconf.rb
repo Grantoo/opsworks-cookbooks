4478cb64714f0be7e27300fe3fb1297ca1b36212
mark <mark@grantoo.org>
2014-03-21 11:44:35 -0700

    add some logs into the recipe cleanconf

--- nginx/recipes/cleanconf.rb
+++ nginx/recipes/cleanconf.rb
@@ -1,5 +1,6 @@
-#ensure that the these directories are emptied before used
+Chef::Log.info "Ensuring that the nginx/conf.d is emptied before use"
 Dir.glob(File.join(node[:nginx][:dir], 'conf.d', "*.conf")).each do |f|
+  Chef::Log.info "removing #{f.to_s}"
   File.delete(f)
 end
 
