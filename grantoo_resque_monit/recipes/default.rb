c9a28493aa0f537a307740d768b98740d50f6d21
mark <mark@grantoo.org>
2014-08-11 14:48:03 -0700

    test deployment configuration restrictions through json settings and layer name

--- grantoo_resque_monit/recipes/default.rb
+++ grantoo_resque_monit/recipes/default.rb
@@ -16,6 +16,17 @@ end
 group = node[:monit][:user_group]
 cpu_count = `cat /proc/cpuinfo | grep processor | wc -l`.to_i # assume linux
 
+if  !node[:opsworks][:instance].nil? && !node[:opsworks][:instance][:layers].nil? && !node[:resque][:layers].nil
+  # scan for matching layer
+  node[:resque][:layers].each do |layer|
+    if node[:opsworks][:instance][:layers].include?(layer[:name])
+      @queues = layer[:queues]
+    end
+  end
+else
+  @queues = node[:resque][:queues]
+end
+
 node[:deploy].each do |application, deploy|
   Dir.glob(File.join(node[:monit][:conf_dir], "grantoo_resque*.monitrc")).each do |f|
     File.delete(f)
@@ -31,7 +42,7 @@ node[:deploy].each do |application, deploy|
           :deploy => deploy,
           :group => group,
           :worker => cpu_number.to_s,
-          :queues => node[:resque][:queues]
+          :queues => @queues
       )
       notifies :restart, resources(:service => "monit"), :delayed
     end
