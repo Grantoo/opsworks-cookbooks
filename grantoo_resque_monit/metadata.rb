06e24c26b1acb8ac626e956f9eedfece05d3ac3c
mark <mark@grantoo.com>
2014-09-08 22:18:47 -0700

    add some dependencies

--- grantoo_resque_monit/metadata.rb
+++ grantoo_resque_monit/metadata.rb
@@ -1,3 +1,6 @@
 maintainer "Grantoo"
 description "Configures monit for the grantoo_resque"
 version "0.1"
+
+depends "deploy"
+depends 'opsworks_commons'
--- grantoo_resque_scheduler_monit/metadata.rb
+++ grantoo_resque_scheduler_monit/metadata.rb
@@ -1,3 +1,6 @@
 maintainer "Grantoo"
 description "Configures monit for the grantoo_resque scheduler"
 version "0.1"
+
+depends "deploy"
+depends 'opsworks_commons'
