136dd7c0aa7e7aa1ee81f07f1f67e70d73f556f9
Kirk <chen.kirk@gmail.com>
2014-09-30 11:37:28 -0700

    Switch to use init.d script and start-stop-daemon to manage java

--- kinesis_to_s3/recipes/default.rb
+++ kinesis_to_s3/recipes/default.rb
@@ -1,22 +1,29 @@
 package "monit"
 
-directory "/opt/kinesis_to_s3" do
+app_root_dir = "/opt/kinesis_to_s3"
+
+directory app_root_dir do
   action :create
 end
 
-remote_file "/opt/kinesis_to_s3/kinesis-to-s3.jar" do
+remote_file File.join(app_root_dir, 'kinesis-to-s3.jar') do
   source "https://github.com/codeflows/kinesis-to-s3/releases/download/0.1/kinesis-to-s3-0.1.jar"
   action :create
 end
 
-template '/opt/kinesis_to_s3/kinesis-to-s3.properties' do
+template File.join(app_root_dir, 'kinesis-to-s3.properties') do
   source 'kinesis-to-s3.properties.erb'
+end
+
+template '/etc/init.d/kinesis-to-s3' do
+  source 'kinesis-to-s3.init.erb'
+  mode 0755
+  owner 'deploy'
 end
 
 template '/etc/monit/conf.d/kinesis-to-s3.monitrc' do
   source 'kinesis-to-s3.monitrc.erb'
 end
-
 
 service "monit" do
   supports :status => false, :restart => true, :reload => true, :start => true
