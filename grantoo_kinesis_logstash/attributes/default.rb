f907212c6b03455e37afdfe5c7c9de3b1c2148b6
Philippe Creux <pcreux@gmail.com>
2014-10-20 14:30:22 -0700

    Fix s3-last-file path

--- grantoo_kinesis_logstash/attributes/default.rb
+++ grantoo_kinesis_logstash/attributes/default.rb
@@ -1,2 +1,2 @@
 default['kinesis_logstash']['input']['region_endpoint'] = "us-east-1"
-default['kinesis_logstash']['input']['sincedb_path'] = "~/.last-s3-file"
+default['kinesis_logstash']['input']['sincedb_path'] = "last-s3-file"
