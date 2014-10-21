73998e772e3bec94226b023c6df4d16405dad80b
Philippe Creux <pcreux@gmail.com>
2014-10-21 10:40:35 -0700

    Document grantoo_kinesis_logstash

--- grantoo_kinesis_logstash/attributes/default.rb
+++ grantoo_kinesis_logstash/attributes/default.rb
@@ -1,2 +1,13 @@
+# The following attributes are required:
+#
+# * ['kinesis_logstash']['input']['aws_credentials'] = [ 'my-key', 'my-secret' ]
+# * ['kinesis_logstash']['input']['bucket'] = 'my-bucket'
+
 default['kinesis_logstash']['input']['region_endpoint'] = "us-east-1"
 default['kinesis_logstash']['input']['sincedb_path'] = "last-s3-file"
+
+default['kinesis_logstash']['output']['host'] = "localhost"
+default['kinesis_logstash']['output']['port'] = "9200"
+default['kinesis_logstash']['output']['user'] = ""
+default['kinesis_logstash']['output']['password'] = ""
+
