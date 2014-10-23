09f04521ecb6621f26f7f474b402ff5ab3278a74
Philippe Creux <pcreux@gmail.com>
2014-10-23 11:02:53 -0700

    Create directory where logstash stores the last_s3_file
    Use new kinesis-to-s3.jar that emits files into date folders
    
    Revert "Use new kinesis-to-s3.jar that emits files into date folders"
    
    This reverts commit ae0afdefc5aeb3895b39c7d881f8c881f8974224.

--- grantoo_kinesis_logstash/recipes/default.rb
+++ grantoo_kinesis_logstash/recipes/default.rb
@@ -7,22 +7,38 @@ name = 'agent'
 
 Chef::Application.fatal!("attribute hash node['logstash']['instance']['#{name}'] must exist.") if node['logstash']['instance'][name].nil?
 
+# Compute and define absolute_sincedb_path
+require 'pathname'
+sincedb_path = node.fetch('kinesis_logstash').fetch('input').fetch('sincedb_path')
+absolute_sincedb_path = if Pathname.new(sincedb_path).absolute?
+                          sincedb_path
+                        else
+                          File.join(
+                            Logstash.get_attribute_or_default(node, name, 'homedir'),
+                            sincedb_path
+                          )
+                        end
+node.default['kinesis_logstash']['input']['absolute_sincedb_path'] = absolute_sincedb_path
+
+directory File.dirname(absolute_sincedb_path) do
+  owner 'logstash'
+  group 'logstash'
+  mode '0644'
+  action :create
+  recursive true
+end
+
 # Generate input file
 logstash_config name do
   input_vars = node.fetch('kinesis_logstash').fetch('input')
-
-  sincedb_path = input_vars.fetch('sincedb_path')
-  unless sincedb_path[0] == '/' # relative path
-    sincedb_path = Logstash.get_attribute_or_default(node, name, 'homedir') + '/' + sincedb_path
-    # default is /var/lib/logstash/last-s3-file
-  end
 
   variables(
     'aws_credentials' => input_vars.fetch('aws_credentials'),
     'bucket'          => input_vars.fetch('bucket'),
     'region_endpoint' => input_vars.fetch('region_endpoint'),
-    'sincedb_path'    => sincedb_path
+    'sincedb_path'    => input_vars.fetch('absolute_sincedb_path')
   )
+
   templates({'100_input_s3.conf' => '100_input_s3.conf.erb'})
   templates_cookbook('grantoo_kinesis_logstash')
   notifies :restart, "logstash_service[#{name}]"
