72ac6f87bdb13f613806771e9c430ad268be722a
Kirk <chen.kirk@gmail.com>
2014-11-21 13:47:46 -0800

    Create redshift folder

---
+++ grantoo_redshift_logstash/metadata.rb
@@ -0,0 +1,8 @@
+name             'grantoo_redshift_logstash'
+maintainer       'Kirk Chen'
+maintainer_email 'kirk@fuelpowered.com'
+license          'all_rights'
+description      'Installs/Configures grantoo_redshift_logstash'
+long_description 'Installs/Configures grantoo_redshift_logstash'
+version          '0.1.0'
+
