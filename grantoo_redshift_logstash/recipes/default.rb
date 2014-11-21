98c80f46c44bbdfc5141c3213551a65e213a8ff9
Kirk <chen.kirk@gmail.com>
2014-11-21 13:47:46 -0800

    Create redshift folder

---
+++ grantoo_redshift_logstash/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: grantoo_redshift_logstash
+# Recipe:: default
+#
+# Copyright (c) 2014 Fuel, All Rights Reserved.
+
+directory "/mnt/ebs1/logstash/redshift-csv/" do
+  owner 'logstash'
+  group 'logstash'
+  mode '0644'
+  action :create
+  recursive true
+end
