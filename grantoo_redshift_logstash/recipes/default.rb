7e3111b2d58ca1738014b71874737f5e2ea15b6d
Kirk <chen.kirk@gmail.com>
2014-11-21 16:44:28 -0800

    directories should have x permission
    directories should have x permission even for others

--- grantoo_redshift_logstash/recipes/default.rb
+++ grantoo_redshift_logstash/recipes/default.rb
@@ -7,15 +7,15 @@
 directory "/mnt/ebs1/logstash/redshift-csv/" do
   owner 'logstash'
   group 'logstash'
-  mode '0644'
+  mode '0755'
   action :create
   recursive true
 end
 
 directory "/mnt/ebs1/logstash/redshift-uniq-csv" do
-  owner 'logstash'
-  group 'logstash'
-  mode '0644'
+  owner 'ubuntu'
+  group 'ubuntu'
+  mode '0755'
   action :create
   recursive true
 end
