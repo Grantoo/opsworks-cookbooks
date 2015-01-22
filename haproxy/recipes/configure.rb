46c80d9d6422f673f9c14ef0d8e962057572c9f4
mark <mark@grantoo.org>
2015-01-22 11:55:32 -0800

    injected info for the goapp backend for haproxy - TEST 2
    fix additional missing bits for the goapp server
    
    add paths for the goapp server v2
    
    fix spacing

--- haproxy/recipes/configure.rb
+++ haproxy/recipes/configure.rb
@@ -3,6 +3,33 @@ service "haproxy" do
   action :nothing # only define so that it can be restarted if the config changed
 end
 
+require 'json'
+Chef::Log.error(node.to_json)
+
+
+# go through these
+# node:opsworks:layersLAYER_NAME:instances
+# fill in these
+#node[:haproxy][:goapp_applications]
+# "goapp": {
+#           "domains": [
+#             "a.example.com",
+#             "b.example.com"
+#           ],
+#           "application_type": "goapp",
+#           "mounted_at": null
+#         }
+# node[:haproxy][:goapp_backends]
+# {
+#     "ip": "10.123.141.159",
+#     "name": "lychee",
+#     "backends": 16
+# }
+
+node[:opsworks][:layers][:goappserver][:instances].each do |server_name, details|
+  node.normal[:haproxy][:goapp_backends] << { :name => server_name, :ip => details[:ip], :backends => 16 }
+end
+
 template "/etc/haproxy/haproxy.cfg" do
   cookbook "haproxy"
   source "haproxy.cfg.erb"
