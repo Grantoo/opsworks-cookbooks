7c93d6f74b0d233d439c2fd03d76ddc1b8a12282
Kirk <chen.kirk@gmail.com>
2015-01-23 18:02:03 -0800

    Redshift pipeline deploy recipe

---
+++ grantoo_redshift_logstash/attributes/deploy.rb
@@ -0,0 +1,4 @@
+default[:opsworks][:deploy_user][:user] = 'ubuntu'
+default[:opsworks][:deploy_user][:group] = 'ubuntu'
+
+deploy[:deploy_to] = '/home/ubuntu'
---
+++ grantoo_redshift_logstash/recipes/deploy.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: grantoo_redshift_logstash
+# Recipe:: deploy
+#
+# Copyright (c) 2015 Fuel, All Rights Reserved.
+
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'other' && application != 'redshift-pipeline'
+    Chef::Log.debug("Skipping grantoo_redshift_logstash::deploy application #{application} as it is not the redshift-pipeline project")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+end
