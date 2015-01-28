772f2b48e82f63dfb96a58b023ed857d70640f05
Kirk <chen.kirk@gmail.com>
2015-01-27 18:29:27 -0800

    Try new deploy method

--- grantoo_redshift_logstash/recipes/deploy.rb
+++ grantoo_redshift_logstash/recipes/deploy.rb
@@ -19,17 +19,52 @@ node[:deploy].each do |application, deploy|
     :ssh_key => deploy[:scm][:ssh_key]
   ) if deploy[:scm][:scm_type].to_s == 'git'
 
-  git '/home/ubuntu/redshift-pipeline' do
+
+  deploy deploy[:deploy_to] do
     repository deploy[:scm][:repository]
-    revision deploy[:scm][:revision]
     user 'ubuntu'
     group 'ubuntu'
-    action :sync
+    migrate deploy[:migrate]
+    migration_command deploy[:migrate_command]
+    environment deploy[:environment].to_hash
+    action deploy[:action]
+
+    case deploy[:scm][:scm_type].to_s
+      when 'git'
+        scm_provider :git
+        enable_submodules deploy[:enable_submodules]
+        shallow_clone deploy[:shallow_clone]
+      else
+        raise "unsupported SCM type #{deploy[:scm][:scm_type].inspect}"
+    end
   end
+
+
+  Chef::Log.info("inside confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
+  Chef::Log.info("inside conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
+
+  Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
+    link "/opt/logstash/agent/etc/conf.d/#{File.basename(to_file)}" do
+      to to_file
+      owner "logstash"
+      group "logstash"
+      action :create
+    end
+  end
+
+
+  # git '/home/ubuntu/redshift-pipeline' do
+  #   repository deploy[:scm][:repository]
+  #   revision deploy[:scm][:revision]
+  #   user 'ubuntu'
+  #   group 'ubuntu'
+  #   action :sync
+  # end
 end
 
-Chef::Log.info("confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
-Chef::Log.info("conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
+
+Chef::Log.info("outside confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
+Chef::Log.info("outside conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
 
 Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
   link "/opt/logstash/agent/etc/conf.d/#{File.basename(to_file)}" do
@@ -39,3 +74,4 @@ Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
     action :create
   end
 end
+
