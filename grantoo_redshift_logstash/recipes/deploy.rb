4b5323e0ed0bd978bff897ceeb22c928e57b8c78
Kirk <chen.kirk@gmail.com>
2015-01-27 17:08:51 -0800

    Added some debug messages

--- grantoo_redshift_logstash/recipes/deploy.rb
+++ grantoo_redshift_logstash/recipes/deploy.rb
@@ -27,6 +27,9 @@ node[:deploy].each do |application, deploy|
     action :sync
   end
 
+  Chef::Log.debug("confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
+  Chef::Log.debug("conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
+
   Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
     link "/opt/logstash/agent/etc/conf.d/#{File.basename(to_file)}" do
       to to_file
