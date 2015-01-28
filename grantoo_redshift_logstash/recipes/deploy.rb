cab20072051a0336893a09374fbe05c45f3baf33
Kirk <chen.kirk@gmail.com>
2015-01-28 14:30:49 -0800

    try symlink

--- grantoo_redshift_logstash/recipes/deploy.rb
+++ grantoo_redshift_logstash/recipes/deploy.rb
@@ -43,6 +43,20 @@ node[:deploy].each do |application, deploy|
   # end
 
 
+  repo =  deploy[:scm][:repository]
+  revision = deploy[:scm][:revision]
+
+  Chef::Log.info("inside git, #{repo}, #{revision}")
+  git '/home/ubuntu/redshift-pipeline' do
+    repository deploy[:scm][:repository]
+    revision deploy[:scm][:revision]
+    user 'ubuntu'
+    group 'ubuntu'
+    notifies :run, "ruby[symlink-conf]", :deplayed
+    action :sync
+  end
+
+
   Chef::Log.info("inside confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
   Chef::Log.info("inside conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
 
@@ -54,38 +68,40 @@ node[:deploy].each do |application, deploy|
       action :create
     end
   end
-
-  repo =  deploy[:scm][:repository]
-  revision = deploy[:scm][:revision]
-
-  Chef::Log.info("inside git, #{repo}, #{revision}")
-  # git '/home/ubuntu/redshift-pipeline' do
-  #   repository deploy[:scm][:repository]
-  #   revision deploy[:scm][:revision]
-  #   user 'ubuntu'
-  #   group 'ubuntu'
-  #   action :sync
-  # end
 end
 
-Chef::Log.info("outside git, #{repo}, #{revision}")
-git '/home/ubuntu/redshift-pipeline' do
-  repository repo
-  revision revision
-  user 'ubuntu'
-  group 'ubuntu'
-  action :sync
-end
+ruby "symlink-conf" do
+  Chef::Log.info("ruby block confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
+  Chef::Log.info("ruby block conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
 
-Chef::Log.info("outside confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
-Chef::Log.info("outside conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
-
-Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
-  link "/opt/logstash/agent/etc/conf.d/#{File.basename(to_file)}" do
-    to to_file
-    owner "logstash"
-    group "logstash"
-    action :create
+  Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
+    link "/opt/logstash/agent/etc/conf.d/#{File.basename(to_file)}" do
+      to to_file
+      owner "logstash"
+      group "logstash"
+      action :create
+    end
   end
 end
-
+#
+# Chef::Log.info("outside git, #{repo}, #{revision}")
+# git '/home/ubuntu/redshift-pipeline' do
+#   repository repo
+#   revision revision
+#   user 'ubuntu'
+#   group 'ubuntu'
+#   action :sync
+# end
+#
+# Chef::Log.info("outside confs count should be 3: #{Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].count}")
+# Chef::Log.info("outside conf.d count should be 1: #{Dir["/opt/logstash/agent/etc/conf.d/"].count}")
+#
+# Dir["/home/ubuntu/redshift-pipeline/logstash/confs/*"].each do |to_file|
+#   link "/opt/logstash/agent/etc/conf.d/#{File.basename(to_file)}" do
+#     to to_file
+#     owner "logstash"
+#     group "logstash"
+#     action :create
+#   end
+# end
+#
