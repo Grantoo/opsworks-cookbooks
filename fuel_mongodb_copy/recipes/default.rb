e4af0ba17d82057bffe19bd410d0c707a3aa8265
mark <mark@fuelpowered.com>
2017-02-02 16:39:13 -0800

    force install of mongodb

--- fuel_mongodb_copy/recipes/default.rb
+++ fuel_mongodb_copy/recipes/default.rb
@@ -4,7 +4,7 @@ bash "install_mongodb" do
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
   echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
   apt-get update
-  apt-get install -y mongodb-org=3.2.11 mongodb-org-server=3.2.11 mongodb-org-shell=3.2.11 mongodb-org-mongos=3.2.11 mongodb-org-tools=3.2.11
+  apt-get install -y --force-yes mongodb-org=3.2.11 mongodb-org-server=3.2.11 mongodb-org-shell=3.2.11 mongodb-org-mongos=3.2.11 mongodb-org-tools=3.2.11
   "mongodb-org hold" | sudo dpkg --set-selections
   "mongodb-org-server hold" | sudo dpkg --set-selections
   "mongodb-org-shell hold" | sudo dpkg --set-selections
