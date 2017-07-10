8bb4d14b0dd1692eadbdc6667e687d08ca40740b
mark <mark@fuelpowered.com>
2017-07-10 15:19:58 -0700

    see if we can install mongo with fuel-copy and then set up replicaset

---
+++ fuel_mongodb_copy/recipes/install.rb
@@ -0,0 +1,14 @@
+bash "install_mongodb" do
+  user 'root'
+  code <<-EOH
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+  echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+  apt-get update
+  apt-get install -y --force-yes mongodb-org=3.2.11 mongodb-org-server=3.2.11 mongodb-org-shell=3.2.11 mongodb-org-mongos=3.2.11 mongodb-org-tools=3.2.11
+  "mongodb-org hold" | sudo dpkg --set-selections
+  "mongodb-org-server hold" | sudo dpkg --set-selections
+  "mongodb-org-shell hold" | sudo dpkg --set-selections
+  "mongodb-org-mongos hold" | sudo dpkg --set-selections
+  "mongodb-org-tools hold" | sudo dpkg --set-selections
+  EOH
+end
