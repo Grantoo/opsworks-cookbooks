9dafeb8fe8a0796436de37301330e5a38f618af1
mark <mark@fuelpowered.com>
2017-07-11 15:45:42 -0700

    correct bug in source template name

--- fuel_mongodb_copy/recipes/configure.rb
+++ fuel_mongodb_copy/recipes/configure.rb
@@ -1,7 +1,7 @@
 
 template File.join("etc", "mongod.conf") do
   mode 0700
-  source "mongo.conf.erb"
+  source "mongod.conf.erb"
   variables(
       :port => node[:mongodb][:config][:port],
       :bind_ip => node[:mongodb][:config][:bindIp],
