111ce782952cb0f864643c58f9ca02da9a42c43b
Jan-Paul Azucena <jp@fuelpowered.com>
2017-07-21 13:26:12 -0700

    [CLEANUP] Removed grantoo_redshift_logstash default script commands necessary for event csv, S3 manipulation, and redshift loading

--- grantoo_redshift_logstash/recipes/default.rb
+++ grantoo_redshift_logstash/recipes/default.rb
@@ -12,35 +12,3 @@ directory "/mnt/logstash/" do
   action :create
   recursive true
 end
-
-# create csv directory
-directory "/mnt/logstash/redshift-csv/" do
-  owner 'logstash'
-  group 'logstash'
-  mode '0755'
-  action :create
-  recursive true
-end
-
-# get s3cmd
-remote_file "/home/ubuntu/s3cmd-1.5.0-rc1.zip" do
-  source "https://github.com/s3tools/s3cmd/archive/v1.5.0-rc1.zip"
-  action :create
-end
-
-# unzip s3cmd
-bash "unzip and move s3cmd" do
-  code <<-EOL
-    unzip /home/ubuntu/s3cmd-1.5.0-rc1.zip -d /home/ubuntu/
-    mv /home/ubuntu/s3cmd-1.5.0-rc1/ /home/ubuntu/s3cmd/
-  EOL
-end
-
-# setup s3cmd
-template "/home/ubuntu/.s3cfg" do
-  source ".s3cfg.erb"
-  owner 'ubuntu'
-  group 'ubuntu'
-  mode '0600'
-  action :create
-end
